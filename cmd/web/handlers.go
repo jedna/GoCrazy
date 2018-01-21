71e4d23ac7d0e8e505aa9ab0980f78cddc62e082
Jan Hrubes <jan@hrubesovi.com>
2018-01-21 18:29:55 +0100

    Initial app commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"fmt"
+	"html/template"
+	"log"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{"./ui/html/base.html", "./ui/html/home.page.html"}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", 500)
+	}
+}
+
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Show's on! #%d", id)
+}
+
+func NewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Let me create that!"))
+}
